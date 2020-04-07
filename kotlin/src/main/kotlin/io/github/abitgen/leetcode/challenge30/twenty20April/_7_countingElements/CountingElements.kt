2497a0f020c6ac54eecf6589e261bc597450e525
abitgen <abitgenius@gmail.com>
2020-04-07 15:56:23 +0530

    April20 - Day7 - Counting Elements

---
+++ kotlin/src/main/kotlin/io/github/abitgen/leetcode/challenge30/twenty20April/_7_countingElements/CountingElements.kt
@@ -0,0 +1,33 @@
+package io.github.abitgen.leetcode.challenge30.twenty20April._7_countingElements
+
+import kotlin.math.max
+import kotlin.math.min
+
+object CountingElements {
+    fun countElements(arr: IntArray): Int {
+        arr.sort()
+        var consecutiveCount = 0
+        val countMap = hashMapOf<Int, Int>()
+        for (i in 0 until arr.size) {
+            if (countMap[arr[i]] == null)
+                countMap[arr[i]] = 1
+            else
+                countMap[arr[i]] = countMap[arr[i]]!! + 1
+        }
+        for (i in 0 until arr.size - 1) {
+            if (arr[i] + 1 == arr[i + 1]) {
+                if (countMap[arr[i]]!! > 1) {
+                    consecutiveCount += if(countMap[arr[i]]!! > countMap[arr[i] + 1]!!) max(countMap[arr[i]]!!, countMap[arr[i] + 1]!!)
+                                        else min(countMap[arr[i]]!!, countMap[arr[i] + 1]!!)
+                } else
+                    consecutiveCount++
+            }
+        }
+        return consecutiveCount
+    }
+}
+
+fun main() {
+    //CountingElements.countElements(intArrayOf(1, 1, 2, 2, 2, 3, 2, 3, 3, 5, 6, 0))
+    CountingElements.countElements(intArrayOf(13,12,9,4,16,7,3,13,14,12,11,4,1,4,9,13,3)) // sorted - 1, 3, 3, 4, 4, 4, 7, 9, 9, 11, 12, 12, 13, 13, 13, 14, 16// 8
+}
